e11e639ee984a00a3f1a247449abd53424108745
zhengrenjie <1354334940@qq.com>
2018-06-04 20:29:21 +0800

    Update WendaApplication.java
    wqe

--- src/main/java/com/zhengrenjie/wenda/WendaApplication.java
+++ src/main/java/com/zhengrenjie/wenda/WendaApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 ////
 @SpringBootApplication
-public class WendaApplication {
+        public class WendaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WendaApplication.class, args);
 	}
+	
+	
 }
